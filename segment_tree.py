47ee2b042df2c03e33cd1147844169d2759eecfc
scrblbug <scrblbug@gmail.com>
2020-10-06 17:40:48 +0900

    segment_treeコメント更新、メソッド整理（投稿用初稿）

--- segment_tree.py
+++ segment_tree.py
@@ -1,18 +1,24 @@
-# 演算内容、単位元の必要に応じて(コンストラクタ参照)
-import math
-
 # セグメント木のクラス
 # 書いた人：scrblbug
 # サイトURL: http://miaoued.net Twitter: @scrblbug
 # とりあえず基本的なところだけを分かりやすい形で組んでみたもの
-# length:データ部の長さ offset:上位構造の長さ tree:全体リスト
 # コンストラクタ (arg1, op, ie):arg1=初期要素or初期要素数
 #       op=演算内容（初期値min的動作） ie=単位元（初期値math.inf）
-# .set(index, value) .rangeq(left, right)範囲指定は半開放 で大体のことはできるはず
+# .length:データ部の長さ
+# .offset:上位構造の長さ
+# .tree:セグメント木全体のリスト
+# .op:演算内容
+# .ie:単位元
+# .set(index, value):値をセットするとともに、必要な部分を再計算する。
+# .get(index):値を取得する。
+# .rangeq(left, right):半開放区間[left, right)での演算結果を取得
+# .allq():全領域での演算結果を取得する。
+
+import math     # 演算内容、単位元の必要に応じて(コンストラクタ参照)
 
 class Segment_Tree:
     # コンストラクタ。リストもしくは要素数にて初期化。
-    # デフォルトでは最小値を求めるが、op(operation=演算内容、デフォルトは最小値)、
+    # デフォルトでは最小値を求めるが、op(operation=演算内容、デフォルトはmin的関数)、
     # ie(identity element単位元)を指定することも可能(f.e: op=operator.add, ie=0)
     def __init__(self, arg1, op=lambda x, y:x if x < y else y, ie=math.inf):
         self.op = op
@@ -51,6 +57,10 @@ class Segment_Tree:
                 break
             else:
                 self.tree[idx] = new
+
+    # データ領域の値をindexに応じて返す
+    def get(self, x):
+        return self.tree[x+self.offset]
         
     # 半開区間[left, right)における欲しい値(アレっすよ、アレ)を求める
     # 一番下の階層からボトムアップ的に必要な部分を見ていく
@@ -67,7 +77,8 @@ class Segment_Tree:
         # 左端と右端が重なるとこまで演算
         # ちょうど重なったところでも、下記の条件分けから一度しか演算されない
         while left <= right:
-            # 左端は、自身が親の右側の子の時(=ノード番号が奇数のとき)にだけ演算する
+            # 左端は、自身が親の右側の子の時(=ノード番号が奇数のとき)にだけ
+            # (自分が左側の時は次の演算に含まれるため)演算する
             if left % 2 == 1:
                 result = self.op(result, self.tree[left])
             # 一段上に移動
@@ -81,14 +92,6 @@ class Segment_Tree:
        
         return result
     
-    # データ領域の値をindexに応じて返す
-    def get(self, x):
-        return self.tree[x+self.offset]
-
     # 全領域を対象にした計算値を返す
     def allq(self):
         return self.tree[1]
-    
-    # 保持している値全体を返す（デバッグ用？）
-    def show_tree(self):
-        return self.tree
