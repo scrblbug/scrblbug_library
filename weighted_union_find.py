3d524a149a6802c7502eb24bd0fa766040ed45c7
scrblbug <scrblbug@gmail.com>
2021-03-08 14:36:23 +0900

    uniteにおける無駄なfind処理を削除

--- weighted_union_find.py
+++ weighted_union_find.py
@@ -44,7 +44,7 @@ class Weighted_Union_Find:
         # そもそも同じグループにいる場合、何もしない
         if lx == ly:
             # が、値が矛盾する場合はエラーを返す
-            if self.get_diff(x, y) != w:
+            if self.weight[y] - self.weight[x] != w:
                 raise ValueError ('value contradiction detected')
             else:
                 return (-1, -1)
