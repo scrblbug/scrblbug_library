86d94a1c36b45918ed4f2651ee0a810c6a562ace
scrblbug <scrblbug@gmail.com>
2021-03-29 11:03:00 +0900

    引数に素数リストを取れるように修正

--- prime_factorization.py
+++ prime_factorization.py
@@ -15,8 +15,9 @@ def get_prime_list(limit):
 # 素因数分解
 # 素数列挙を行った後、それをもとに素因数分解を行い、
 # (素数, 指数) のリストを返す
-def prime_factorization(n):
-    primes = get_prime_list(int(n**0.5))
+def prime_factorization(n, primes=[]):
+    if not primes or primes[-1] < int(n**0.5):
+        primes = get_prime_list(int(n**0.5))
     result = []
     for p in primes:
         if p >= n:
